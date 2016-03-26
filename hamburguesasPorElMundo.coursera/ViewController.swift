dfc605c0181d6533558343482022c5de39d4b4ce
juanromero3 <jromero2397@gmail.com>
2016-03-26 19:15:07 +0100

    Initial Commit

---
+++ hamburguesasPorElMundo.coursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hamburguesasPorElMundo.coursera
+//
+//  Created by Juan Romero on 26/3/16.
+//  Copyright © 2016 Juan Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
